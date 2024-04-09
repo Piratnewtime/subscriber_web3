2c9980594e93f18e1cdf6585530a72f3ebe49dc7
Pirate newTime <himyl@ya.ru>
2024-04-09 22:56:40 +0400

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/subscriber_web3",
+    //basePath: "/subscriber_web3",
     crossOrigin: "anonymous",
     output: "export",
     reactStrictMode: true,
