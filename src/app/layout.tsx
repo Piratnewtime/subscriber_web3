62238d05411e8059a427470ed61b59bf9c4de057
Pirate newTime <himyl@ya.ru>
2024-07-01 12:48:37 +0400

    add analytic tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 //import { WalletWrapper } from "./wallet";
@@ -40,6 +41,29 @@ export default function RootLayout({
             {children}
           </Box>
         </WalletWrapper>
+        <Script type="text/javascript">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(97712486, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+          });`}
+        </Script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/97712486" style={{ position: 'absolute', left: '-9999px' }} alt="" /></div></noscript>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HNCKZX4ESJ"></Script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-HNCKZX4ESJ');`}
+        </Script>
       </body>
     </html>
   );
